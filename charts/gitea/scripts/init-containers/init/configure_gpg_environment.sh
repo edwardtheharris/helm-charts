d19367f4d943746c023b549dba053d12517524ba
Xander Harris <xandertheharris@gmail.com>
2026-09-01 23:49:59 -0700

    chore: cleanup broken gitea
    Closes #144

--- charts/gitea/scripts/init-containers/init/configure_gpg_environment.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-gpg --batch --import "$TMP_RAW_GPG_KEY"
--- charts/gitea/unittests/bash/test_helper/common-setup.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-function common_setup() {
-  load "$TEST_ROOT/test_helper/bats-support/load"
-  load "$TEST_ROOT/test_helper/bats-assert/load"
-  load "$TEST_ROOT/test_helper/bats-mock/stub"
-}
